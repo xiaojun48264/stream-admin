d44d30bb5302546c8f49537f17d51b62097320bd
xiaojun <1134241639@qq.com>
2023-04-22 14:40:50 +0800

    1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -24,25 +24,17 @@ const count = ref(0)
 
   <p>
     Check out
-    <a
-      href="https://vuejs.org/guide/quick-start.html#local"
-      target="_blank"
-    >create-vue</a>, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    starter
   </p>
   <p>
     Install
-    <a
-      href="https://github.com/vuejs/language-tools"
-      target="_blank"
-    >Volar</a>
+    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
     in your IDE for a better DX
   </p>
-  <p class="read-the-docs">
-    Click on the Vite and Vue logos to learn more
-  </p>
-  <a-button type="primary">
-    测试
-  </a-button>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <a-button type="primary"> 测试 </a-button>
+  <a-input placeholder="请填写"></a-input>
 </template>
 
 <style scoped>
